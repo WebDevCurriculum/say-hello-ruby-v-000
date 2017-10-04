2528e3c06e73d662a91d743bff4efd9c6e10d04f
Charity Youngblood <youngblood.charity@gmail.com>
2017-10-04 17:46:16 +0000

    automatically backed up by learn

--- say_hello.rb
+++ say_hello.rb
@@ -1,7 +1,7 @@
 # Build your say_hello method here
 #method should take two arguments - one required and one optional/default
 # be sure to place the default argument at the end of the argument list
-def say_hello(arg1 = "Ruby Programmer")
+def say_hello(name = "Ruby Programmer")
   puts "Hello #{arg1}!"
 end
 
